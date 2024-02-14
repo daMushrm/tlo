75eef84dc1ac9181777e5b8789a3de23251f90ce
M1ghtyMushroom <hilariousbananas@proton.me>
2024-02-15 01:53:09 +0200

    Just centered the text

--- resources/views/components/grade-card.blade.php
+++ resources/views/components/grade-card.blade.php
@@ -3,10 +3,10 @@
 <div>
     <a href="{{ route('grades.show', ['grade' => $grade]) }}" class="w-full">
         <div
-            class="rounded-lg z-10 relative border text-card-foreground bg-white shadow-xl flex justify-between items-center">
-            <div class="p-6">
+            class="rounded-lg z-10 relative border text-card-foreground bg-white shadow-xl flex justify-between items-center w-full">
+            <div class="p-6 w-full">
                 @if (isset($head))
-                <div class="flex items-center">
+                <div class="flex justify-center w-full">
                     <h3 class="sm:text-3xl text-xl">
                         {{ $head }}
                     </h3>
